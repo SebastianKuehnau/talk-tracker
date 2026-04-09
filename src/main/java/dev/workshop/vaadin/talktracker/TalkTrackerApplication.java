defa81f622ea600b435d829f639f72aaf87aba02
Sebastian Kühnau <seb@vaadin.com>
2026-04-09 12:42:28 +0200

    switch to Java 25

---
+++ src/main/java/dev/workshop/vaadin/talktracker/TalkTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.workshop.vaadin.talktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkTrackerApplication.class, args);
+	}
+
+}
