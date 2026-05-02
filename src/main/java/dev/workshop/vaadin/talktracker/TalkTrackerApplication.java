b80a4f53b2b083793c073a7e47cafef4d38b9c74
Sebastian Kühnau <seb@vaadin.com>
2026-05-02 10:11:27 +0200

    Expanded `DataInitializer` to include a comprehensive set of talks and workshops for the 2026 schedule, replacing categories with detailed data initialization for sessions.

--- src/main/java/dev/workshop/vaadin/talktracker/TalkTrackerApplication.java
+++ src/main/java/dev/workshop/vaadin/talktracker/TalkTrackerApplication.java
@@ -3,14 +3,13 @@ package dev.workshop.vaadin.talktracker;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.aura.Aura;
-import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
+@PWA(name = "Talk Tracker", shortName = "Talk Tracker")
 @Push
-@EnableScheduling
 @StyleSheet(Aura.STYLESHEET)
 @SpringBootApplication
 public class TalkTrackerApplication implements AppShellConfigurator {
