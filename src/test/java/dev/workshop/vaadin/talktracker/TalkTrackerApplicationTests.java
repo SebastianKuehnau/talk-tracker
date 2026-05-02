d1573abed85f9b3186137e60bb458ce504a87e04
Sebastian Kühnau <seb@vaadin.com>
2026-05-02 11:09:52 +0200

    Initialize Vaadin project with Spring Boot and Maven configuration

---
+++ src/test/java/dev/workshop/vaadin/talktracker/TalkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.workshop.vaadin.talktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
