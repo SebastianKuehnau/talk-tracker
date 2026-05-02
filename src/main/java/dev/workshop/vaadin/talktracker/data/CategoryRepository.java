91ea7fbbf576a07257346356fe9d027416bbc9ea
Sebastian Kühnau <seb@vaadin.com>
2026-05-02 10:10:49 +0200

    Remove all unused classes, services, and UI components related to talk tracking.

--- src/main/java/dev/workshop/vaadin/talktracker/data/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.workshop.vaadin.talktracker.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
