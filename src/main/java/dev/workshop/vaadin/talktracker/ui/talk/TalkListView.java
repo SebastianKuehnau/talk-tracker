c3cef829d7fa0228096d6d71b90dc687348d3978
Sebastian Kühnau <seb@vaadin.com>
2026-05-02 11:56:22 +0200

    Add filter functionality to TalkListView

--- src/main/java/dev/workshop/vaadin/talktracker/ui/talk/TalkListView.java
+++ src/main/java/dev/workshop/vaadin/talktracker/ui/talk/TalkListView.java
@@ -1,21 +1,35 @@
 package dev.workshop.vaadin.talktracker.ui.talk;
 
+import com.vaadin.flow.component.AbstractField;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import dev.workshop.vaadin.talktracker.data.Talk;
 import dev.workshop.vaadin.talktracker.data.TalkRepository;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Route("")
 public class TalkListView extends VerticalLayout {
 
+    private final List<Talk> allTalks;
+    private final Grid<Talk> grid;
+
     public TalkListView(TalkRepository talkRepository) {
-        var grid = new Grid<>(Talk.class);
-        grid.setItems(talkRepository.findAll());
+        var filterField = new TextField("", "filter for ...");
+        filterField.addValueChangeListener(this::onFilter);
+        filterField.setValueChangeMode(ValueChangeMode.TIMEOUT);
+        filterField.setValueChangeTimeout(500);
+        filterField.setWidthFull();
+
+        grid = new Grid<>(Talk.class);
+        allTalks = talkRepository.findAll();
+        grid.setItems(allTalks);
 
         grid.setColumns("title", "speaker", "room");
         grid.getColumnByKey("room").setFlexGrow(0).setSortable(true).setHeader("Room");
@@ -33,6 +47,16 @@ public class TalkListView extends VerticalLayout {
         grid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
 
         grid.setSizeFull();
-        add(grid);
+        add(filterField, grid);
+    }
+
+    private void onFilter(AbstractField.ComponentValueChangeEvent<TextField, String> event) {
+        var lowerCaseValue = event.getValue().toLowerCase();
+        var filteredTalks = allTalks.stream()
+                .filter(talk -> talk.getTitle().toLowerCase().contains(lowerCaseValue) ||
+                        talk.getSpeaker().toLowerCase().contains(lowerCaseValue) ||
+                        talk.getRoom().toLowerCase().contains(lowerCaseValue))
+                .toList();
+        grid.setItems(filteredTalks);
     }
 }
