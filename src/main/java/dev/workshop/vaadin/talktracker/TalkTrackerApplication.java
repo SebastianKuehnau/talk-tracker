0235047403597ed28ba5bf4fd1c0227e9db3546b
Sebastian Kühnau <seb@vaadin.com>
2026-05-03 19:41:58 +0200

    Added Aura theme and AppShellConfigurator implementation in TalkTrackerApplication

--- src/main/java/dev/workshop/vaadin/talktracker/TalkTrackerApplication.java
+++ src/main/java/dev/workshop/vaadin/talktracker/TalkTrackerApplication.java
@@ -1,10 +1,15 @@
 package dev.workshop.vaadin.talktracker;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.aura.Aura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@StyleSheet(Aura.STYLESHEET)
 @SpringBootApplication
-public class TalkTrackerApplication {
+public class TalkTrackerApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalkTrackerApplication.class, args);
